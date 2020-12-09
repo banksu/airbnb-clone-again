2874d2aa64f03b6d4b54b36f10f41be5e78942d9
banksu <67863190+banksu@users.noreply.github.com>
2020-12-09 10:03:15 +0900

    CharField : choices

--- users/models.py
+++ users/models.py
@@ -6,7 +6,17 @@ from django.db import models
 
 class User(AbstractUser):
 
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        ((GENDER_MALE, "Male"),
+         (GENDER_FEMALE, "Female"),
+            (GENDER_OTHER, "Other"))
+    )
+
     """Custum User Model"""
-    avatar = models.ImageField()
-    gender = models.CharField(max_length=10, null=True)
+    avatar = models.ImageField(null=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
     bio = models.TextField(default="")
