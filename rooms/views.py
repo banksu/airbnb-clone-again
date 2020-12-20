bf90864bc3adcf13029b3e175f77313d64b44484
banksu <67863190+banksu@users.noreply.github.com>
2020-12-21 03:45:33 +0900

    function view (room)

--- rooms/views.py
+++ rooms/views.py
@@ -1,48 +1,34 @@
+from django.http import Http404
+from django.views.generic import ListView
+from django.urls import reverse
 from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 # Create your views here.
 
 
+class HomeView(ListView):
 
-class HomeView
+    """HomeView Difinition"""
 
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
 
-
-# def all_rooms(request):
-#     page = request.GET.get("page", 1)
-#     room_list = models.Room.objects.all()
-#     paginator = Paginator(room_list, 10, orphans=5)
-#     try:
-#         rooms = paginator.page(int(page))
-#         return render(
-#             request,
-#             "rooms/home.html",
-#             context={"page": rooms},
-#         )
-#     except EmptyPage:
-#         rooms = paginator.page(1)
-#         return redirect("/")
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     now = timezone.now()
+    #     context["now"] = now
+    #     return context
 
 
-
-
-    # def all_rooms(request):
-    #     page = request.GET.get("page", 1)
-    # page = int(page or 1)
-    # page_size = 10
-    # limit = page_size * page
-    # offset = limit - page_size
-    # all_rooms = models.Room.objects.all()[offset:limit]
-    # page_count = ceil(models.Room.objects.count() / page_size)
-    # return render(
-    #     request,
-    #     "rooms/home.html",
-    #     context={
-    #         "rooms": all_rooms,
-    #         "page": page,
-    #         "page_count": page_count,
-    #         "page_range": range(1, page_count + 1),
-    #     },
-    # )
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404()
+        # return redirect(reverse("core:home"))
+    # print(room)
