3b864398f91e60af099db554e33ab714fb544661
banksu <67863190+banksu@users.noreply.github.com>
2020-12-22 18:48:51 +0900

    #13.6 .....ㅇ<-<
    Amenity and facility filter is wrong

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,6 @@
-from django.http import Http404
-from django.views.generic import ListView
-from django.urls import reverse
-from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 # Create your views here.
@@ -17,18 +16,98 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     now = timezone.now()
-    #     context["now"] = now
-    #     return context
+
+class RoomDetail(DetailView):
+    model = models.Room
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
-        # return redirect(reverse("core:home"))
-    # print(room)
+def search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
+    select_amenities = request.GET.getlist("amenities")
+    select_facilities = request.GET.getlist("facilities")
+    print(select_amenities, select_facilities)
+
+    form = {
+        "city": city,
+        "select_country": country,
+        "select_room_type": room_type,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "select_amenities": select_amenities,
+        "select_facilities": select_facilities,
+        "instant": instant,
+        "superhost": superhost,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    if room_type != 0:
+        filter_args["room_type__pk"] = room_type
+
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    # 13.6 Amenity and facility filter is wrong
+    if len(select_amenities) < 0:
+        for select_amenity in select_amenities:
+            filter_args["amenities__pk"] = int(select_amenity)
+
+    if len(select_facilities) < 0:
+        for select_facility in select_facilities:
+            filter_args["facilities__pk"] = int(select_facility)
+
+        print(filter_args)
+    rooms = models.Room.objects.filter(**filter_args)
+
+    return render(
+        request,
+        "rooms/search.html",
+        {**form, **choices, "rooms": rooms},
+    )
