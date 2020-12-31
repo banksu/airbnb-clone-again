cd72c0a82d24b2dc74868cd4a5241c5b38767f47
banksu <67863190+banksu@users.noreply.github.com>
2020-12-31 18:10:08 +0900

    email Environments

--- users/models.py
+++ users/models.py
@@ -44,6 +44,11 @@ class User(AbstractUser):
         choices=CURENCY_CHOICES, max_length=3, blank=True, default=CURENCY_KRW
     )
     superhost = models.BooleanField(default=False)
+    email_confirmed = models.BooleanField(default=False)
+    email_secret = models.CharField(max_length=120, default="", blank=True)
 
     def __str__(self):
         return self.username + " night"
+
+    def verify_email(self):
+        pass
