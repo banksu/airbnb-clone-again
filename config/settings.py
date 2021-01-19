8ef84922a7b2cdee9a0db2b096fc3dd93f6ca826
banksu <67863190+banksu@users.noreply.github.com>
2021-01-19 10:48:57 +0900

    fail email verify....

--- config/settings.py
+++ config/settings.py
@@ -148,6 +148,7 @@ MEDIA_URL = "/media/"
 # Email Configuration
 
 EMAIL_HOST = "smtp.mailgun.org"
-EMAIL_PORT = "587"
+EMAIL_PORT = "465"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
 EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "noReply@sandbox19bc0aa2da2941998ce1afc609ce17ce.mailgun.org"
